e8e5a169effad829fef92afd3f15bc131e0e1a3e
Shahriz Malek <malekshahriz15@gmail.com>
2018-12-12 01:43:19 +0000

    Day 1: Added The songs, img_links, artists, songLengths, and links.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,54 @@
 
 // BELOW Update the songs to your favorites songs. There should be at least 4.
 var songs = [
-    "24 Karat Magic",
-    "Nice For What",
-    "Universe"
+    "Noticed",
+    "Fu Shit",
+    "Look Back At It",
+    "From the D to The A",
+    "Every Day We Lit",
+    "Want You"
 ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
+var images_links =[
+    "https://i.ytimg.com/vi/nceqQyqIa5o/maxresdefault.jpg",
+    "https://images.genius.com/c138b9f279b14338c42321280728e350.999x999x1.png",
+    "https://ssle.ulximg.com/image/750x750/cover/1544172196_2b946c1b1740419f2f52a05894015c48.jpg/8896cab0a1d18fa4236a30396bcf9022/1544172196_c0cbe332eb87548eecfcf3dbdb142e18.jpg",
+    "https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F82cf56f3623008d82b006dbec40941d2.480x480x1.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/YNFLucci.jpg/220px-YNFLucci.jpg",
+    "https://i.ytimg.com/vi/ogRCYa6-WSA/maxresdefault.jpg"
+];
 
+var artists =[
+    "Lil Mosey",
+    "Lil Mosey",
+    "A Boogie Wit da Hoodie",
+    "Tea Grizzley and Lil Yachty",
+    "YFN Lucci and PnB Rock",
+    "Rynx and Miranda Glory"
+    
+];
+
+var songLengths = [
+    "2:52",
+    "2:38",
+    "3:00",
+    "2:50",
+    "3:30",
+    "3:36"
+        
+]; 
+
+var links = [
+    "https://www.youtube.com/watch?v=nceqQyqIa5o", 
+    "https://www.youtube.com/watch?v=AvncOx_lnGQ",
+    "https://www.youtube.com/watch?v=DmNfT-B7nlA",
+    "https://www.youtube.com/watch?v=-S0aea777jM",
+    "https://www.youtube.com/watch?v=44Vk5KyQbiA",
+    "https://www.youtube.com/watch?v=ogRCYa6-WSA"
+    
+];
 
 
 
